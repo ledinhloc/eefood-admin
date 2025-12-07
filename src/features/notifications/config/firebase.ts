25cee7f109e640a9d8bd59d28db7ff9bb41580fe
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-12-07 20:21:01 +0700

    feat(): add firebase.ts

---
+++ src/features/notifications/config/firebase.ts
@@ -0,0 +1,43 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export const requestForToken = async () => {
+  try {
+    const currentToken = await getToken(messaging, {
+      vapidKey: import.meta.env.REACT_APP_FIREBASE_VAPID_KEY,
+    });
+
+    if (currentToken) {
+      console.log('FCM Token:', currentToken);
+      return currentToken;
+    } else {
+      console.log('No registration token available.');
+      return null;
+    }
+  } catch (err) {
+    console.error('An error occurred while retrieving token:', err);
+    return null;
+  }
+};
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+  });
+
+export default app;
